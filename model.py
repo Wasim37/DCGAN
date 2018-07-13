55b25b88aa8ce668ebbb3fe29b668f76e07cd8e7
wasim <wasim37@163.com>
2018-07-13 15:28:05 +0800

    commit

---
+++ model.py
@@ -0,0 +1,29 @@
+import os,sys
+import numpy as np
+import time
+from glob import glop
+
+
+class DCGAN(object):
+    """"""
+
+    def __init__(self, sess, input_height=108, input_width=108, crop=True,
+         batch_size=64, sample_num = 64, output_height=64, output_width=64,
+         y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
+         gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
+         input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, data_dir='./data'):
+        """
+        Args:
+          sess: TensorFlow session
+          y_dim: (optional) Dimension of dim for y. [None]
+          z_dim: (optional) Dimension of dim for Z. [100]
+          gf_dim: (optional) Dimension of gen filters in first conv layer. [64]
+          df_dim: (optional) Dimension of discrim filters in first conv layer. [64]
+          gfc_dim: (optional) Dimension of gen units for for fully connected layer. [1024]
+          dfc_dim: (optional) Dimension of discrim units for fully connected layer. [1024]
+          c_dim: (optional) Dimension of image color. For grayscale input, set to 1. [3]
+        """
+        
+        
+    
+    
